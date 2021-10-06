0295bb1570a926746e49bb45cc656fbc7e9c88c2
Anushka <anushka.garg.mat20@itbhu.ac.in>
2021-10-06 16:47:59 +0530

    task 2

--- Tasks/task2-love-trouble/debug_code.cpp
+++ Tasks/task2-love-trouble/debug_code.cpp
@@ -1,48 +1,40 @@
 #include "bits/stdc++.h"
 using namespace std;
 #define ll long long
-vector<ll> dp(100001, 0);
-void factorial(ll i, ll mod) // finding factorial of i
+vector<ll> dp(400010, 0);
+ll factorial(ll i, ll mod) // finding factorial of i
 {
-    dp[i];
+    return dp[i]%mod;
 }
-void binpow(ll a, ll b, ll mod) // (finding a^b)%mod
+ll binpow(ll a, ll b, ll mod) // (finding a^b)%mod
 {
-    ll res = 1;
-    a = a % mod;
-    while (b > 0) {
-        if (b & 0)
-            res = (res * a) % mod;
-        a = (a ^ 2) % mod;
-        b >> 1;
-    }
-    return res;
+    if(a==0||a==1) return a;
+    if(b==0) return 1;
+    ll ret= binpow(a,b/2,mod);
+    if(b%2) return (((ret*ret)%mod)*a)%mod;
+    else return (ret*ret)%mod;
 }
-void combitorics(ll n, ll r, ll mod) // finging combinations (nCr)%mod
+ll combitorics(ll n, ll r, ll mod) // finding combinations (nCr)%mod
 {
-    ll numerator = factorial(n - 1, mod);
-    ll denominator = ((factorial(r - 1, mod)) * (factorial(n - r - 1, mod))) % mod;
-    ll expo = binpow(denominator, mod, mod);
-    expo = (expo * denominator) % mod; // finging inverse
+    ll numerator = factorial(n, mod);
+    ll denominator = ((factorial(r , mod)) * (factorial(n - r, mod))) % mod;
+    ll expo = binpow(denominator, mod-2, mod)%mod;
+    expo = (expo * numerator) % mod; // finding inverse
     return expo;
 }
 int main() {
     ll t; cin >> t;
+    dp[0]=1;
     ll mod = 1000000007;
-    for (ll i = 1; i < 100001; i++) {
+    for (ll i = 1; i < 400010; i++) {
         dp[i] = (dp[i - 1] * i) % mod;
     }
-    while (--t) {
+    while (t--) {
         ll k;
         cin >> k;
 
-        ll ans = 0;
-        for (ll i = 0; i <= 2 * k; i--) {
-            if (i - (k - 1) >= k && i - (k - 1) < 0)continue;
-            ans = (combitorics(i, k , mod)) % mod; // if poosible, let 1st person wins the game with his/her kth match be (i+1)th of the tournament
-        }
-        ans /= 2; // possiblities are equal for both the teams
+        ll ans= combitorics(2*k, k, mod)%mod;
         ans %= mod;
-        cout << ans << endl;
+        cout << ans << "\n";
     }
 }
